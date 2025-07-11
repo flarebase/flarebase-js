d97117b69378e6ae8b8e4ba657a1c815ffa97bb8
chayakorn2002 <chayakorn.chien@gmail.com>
2025-07-12 06:51:26 +0700

    ✨ Refactor Structure.

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,22 +1,19 @@
-import type { Fetch } from "./types";
+import type { Fetch } from './types';
 
-const resolveFetch = (customFetch?: Fetch): Fetch => {
-    return (...args) => (customFetch ?? fetch)(...args);
-};
+function resolveFetch(customFetch?: Fetch): Fetch {
+  return (...args) => (customFetch ?? fetch)(...args);
+}
 
-export const fetchWithAuth = (
-    apiKey: string,
-    customFetch?: Fetch,
-): Fetch => {
-    const _fetch = resolveFetch(customFetch);
+export function fetchWithAuth(apiKey: string, customFetch?: Fetch): Fetch {
+  const _fetch = resolveFetch(customFetch);
 
-    return async (input, init) => {
-        const headers = new Headers(init?.headers);
+  return async (input, init) => {
+    const headers = new Headers(init?.headers);
 
-        if (!headers.has("x-api-key")) {
-            headers.set("x-api-key", apiKey);
-        }
+    if (!headers.has('x-api-key')) {
+      headers.set('x-api-key', apiKey);
+    }
 
-        return _fetch(input, { ...init, headers });
-    };
-};
+    return _fetch(input, { ...init, headers });
+  };
+}
