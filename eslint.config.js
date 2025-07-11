f7a394bd044247fc61ab73907b179130878b92c9
chayakorn2002 <chayakorn.chien@gmail.com>
2025-07-12 06:49:14 +0700

    ✨ Refactor Structure.

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import nyxb from '@nyxb/eslint-config';
+
+const compat = new FlatCompat();
+
+export default nyxb({
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+  },
+  ignores: ['node_modules', 'dist'],
+}, ...compat.config({
+  extends: [],
+  rules: {
+  },
+}));
